d0572e6d88222f3ecaaa71423edfc0ff0a84bdda
heydarovvali <110536438+heydarovvali@users.noreply.github.com>
2024-06-23 11:13:02 +0400

    computer store project created

---
+++ computer-store/src/main/java/az/vali/computer_store/ComputerStoreApplication.java
@@ -0,0 +1,13 @@
+package az.vali.computer_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComputerStoreApplication.class, args);
+	}
+
+}
